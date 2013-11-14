be4bdfcd779a6420bf50bec11975e3a5276d72ff
Atomeh <atomeh@pehnes.server>
2013-11-14 21:29:25 +0400

    ZOPOROZHEYC

---
+++ lua/acf/shared/engines/v4.lua
@@ -0,0 +1,19 @@
+
+-- V4 engines
+
+ACF_DefineEngine( "1.2-V4", {
+	name = "1.2L V4 Petrol",
+	desc = "Zapor",
+	model = "models/engines/v6small.mdl",
+	sound = "acf_engines/i4_petrolsmall2.wav",
+	category = "V4",
+	fuel = "Petrol",
+	enginetype = "GenericPetrol",
+	weight = 70,
+	torque = 70,
+	flywheelmass = 0.04,
+	idlerpm = 900,
+	peakminrpm = 3000,
+	peakmaxrpm = 4400,
+	limitrpm = 5000
+} )
